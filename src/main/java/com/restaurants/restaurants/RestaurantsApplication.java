e803b19a9b1f7aab2a9bc743ba28a772cef33040
virendrasharma1 <vs82347@gmail.com>
2018-12-28 12:58:36 +0530

    vs - first commit

---
+++ src/main/java/com/restaurants/restaurants/RestaurantsApplication.java
@@ -0,0 +1,14 @@
+package com.restaurants.restaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsApplication.class, args);
+	}
+
+}
+
