aa0126501c6025af1505ec7fdb6af0dafda51eec
virendra.sharma-at-579062553257 <A/rgR+OJgwy1MgRrMhQHeNr99psI0VsvjFztWoVbY1k=>
2019-03-15 16:08:03 +0530

    VS - Restaurant profile

--- src/main/java/com/restaurants/RestaurantsApplication.java
+++ src/main/java/com/restaurants/RestaurantsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -18,7 +20,11 @@ import java.util.Collections;
 @PropertySource("classpath:hibernate.properties")
 @PropertySource("classpath:log4j.properties")
 public class RestaurantsApplication {
-
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(RestaurantsApplication.class, args);
 	}
