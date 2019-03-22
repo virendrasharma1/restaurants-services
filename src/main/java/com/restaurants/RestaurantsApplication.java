cb4143bbd3c17532488aab79b63e502d7bfa88b9
virendra.sharma-at-579062553257 <A/rgR+OJgwy1MgRrMhQHeNr99psI0VsvjFztWoVbY1k=>
2019-03-22 16:40:40 +0530

    VS -  Build gradle jdbc dependency

--- src/main/java/com/restaurants/RestaurantsApplication.java
+++ src/main/java/com/restaurants/RestaurantsApplication.java
@@ -20,20 +20,20 @@ public class RestaurantsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantsApplication.class, args);
 	}
-//	@Bean
-//	public CorsFilter corsFilter() {
-//		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		final CorsConfiguration config = new CorsConfiguration();
-//		config.setAllowCredentials(true);
-//		config.setExposedHeaders(Arrays.asList("Content-Type", "Accept",
-//				"X-com-restaurant-id", "X-com-auth-token","X-com-device-id","X-com-ip-address", "X-com-latitude","X-com-longitude"));
-//		config.setAllowedOrigins(Collections.singletonList("*"));
-//		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept","Content-Type", "Accept",
-//				"X-com-restaurant-id", "X-com-auth-token","X-com-device-id","X-com-ip-address", "X-com-latitude","X-com-longitude"));
-//		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-//		source.registerCorsConfiguration("/**", config);
-//		return new CorsFilter(source);
-//	}
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setExposedHeaders(Arrays.asList("Content-Type", "Accept",
+				"X-com-restaurant-id", "X-com-auth-token","X-com-device-id","X-com-ip-address", "X-com-latitude","X-com-longitude"));
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept","Content-Type", "Accept",
+				"X-com-restaurant-id", "X-com-auth-token","X-com-device-id","X-com-ip-address", "X-com-latitude","X-com-longitude"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 
 }
 
